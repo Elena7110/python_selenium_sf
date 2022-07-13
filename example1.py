d9e260d78331d2930f952ff712aad4a23acf59a6
Elena7110 <alen.spirit@gmail.com>
2022-07-13 11:32:57 +0300

    move to pets home

--- example1.py
+++ example1.py
@@ -7,9 +7,12 @@ from time import sleep
 
 driver = webdriver.Chrome("/Users/Admin/Desktop/python_selenium_sf/chromedriver")
 
-driver.get('https://google.com')
+driver.get('http://130.193.37.179/app/pets')
 
-driver.find_element(By.XPATH, "//input[@title=\"Поиск\"]").send_keys('Skillfactory' + Keys.RETURN)
-sleep(2)
-driver.save_screenshot('sf.png')
+(driver.find_elements(By.XPATH, "//*[@id=\"image\"]/img"))[0].click()
+
+
+
+sleep(3)
+driver.save_screenshot('pet_home.png')
 driver.quit()
