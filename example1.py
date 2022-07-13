d2513ec26dcc92935947a4c4a50645bd677d76db
Elena7110 <alen.spirit@gmail.com>
2022-07-13 10:47:48 +0300

    edition of application

---
+++ example1.py
@@ -0,0 +1,15 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from time import sleep
+
+driver = webdriver.Chrome("/Users/Admin/Desktop/python_selenium_sf/chromedriver")
+
+driver.get('https://google.com')
+
+driver.find_element(By.XPATH, "//input[@title=\"Поиск\"]").send_keys('Skillfactory' + Keys.RETURN)
+sleep(2)
+driver.save_screenshot('sf.png')
+driver.quit()
